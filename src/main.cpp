9360a1ff18f4210b67ff6fdbbfcd47ca1cfb08bd
alexey907 <leha.yarmolenko@gmail.com>
2026-04-30 11:18:24 -0700

    enabled log prints

--- src/main.cpp
+++ src/main.cpp
@@ -71,7 +71,7 @@ void StartBlinkTask(void *argument) {
     
     for(;;) {
         HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_2);
-        //usb.println("LED toggled");
+        usb.println("LED toggled");
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
